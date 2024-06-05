be5e5f42b792866675efd27f2beb854e67653f76
Tacit27 <116271211+Tacit27@users.noreply.github.com>
2024-06-05 11:18:39 +0800

    Update 主页.py

--- 主页.py
+++ 主页.py
@@ -20,27 +20,4 @@ st.title('TALL')
 st.subheader("Taike Artificial inteLligence Lab", divider='rainbow')
 st.write('TALL隶属于南京理工大学泰州科技学院计算机科学与工程学院，负责人为姜枫教授。"TALL" 的全称是"Taike Artificial InteLligence Laboratory（泰科人工智能实验室）"。TALL的主要研究兴趣包括机器学习、模式识别、计算机视觉、自然语言处理等领域。TALL面向本校常年招纳优秀学子，为本科生提供系统的科研训练，如果您希望加入我们或希望在TALL做本科毕业设计请与请与实验室老师联系。')
 
-test_items = [
-    dict(
-        title="",
-        text="",
-        img="images/主页图.jpg"
-    ),
-    dict(
-        title="",
-        text="",
-        img="images/主页图2.jpg"
-    ),
-    dict(
-        title="",
-        text="",
-        img="images/主页图3.jpg"
-    ),dict(
-        title="",
-        text="",
-        img="images/主页图4.jpg"
-    ),
-]
-
-carousel(items=test_items, width=0.9)
-
+st.image('images/主页图.jpg')
